77109a70b4497cf20da836344a97d1b43e769a8e
Nahom Assefa <nzenebe@gmail.com>
2022-03-10 16:21:32 -0600

    auth validation

--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { useMutation } from '@apollo/client';
 import { ADD_USER } from '../utils/mutations';
+import Auth from '../utils/auth';
 
 // import Auth from '../utils/auth';
 
@@ -32,7 +33,7 @@ const Signup = () => {
       });
 
       console.log('data', data);
-      // Auth.login(data.addUser.token);
+      Auth.login(data.addUser.token);
     } catch (e) {
       console.error(e);
     }
